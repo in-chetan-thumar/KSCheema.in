1b97e7fc8b58073fdbe739c31b10d3649c981ff2
in-chetan-thumar <chetan.thumar@tiez.nl>
2023-03-02 18:25:11 +0530

    Commit news page changes

--- www/resources/views/front/media.blade.php
+++ www/resources/views/front/media.blade.php
@@ -19,10 +19,36 @@
 
     <div class="latest-news">
         <div class="container">
-            <div class="title">
-                <h3>latest news</h3>
-            </div>
             <div class="latest-news-inner">
+                
+                
+                <div class="latest-news-box">
+                    <div class="images-box">
+                        <div class="img-destop">
+                            <img src="front/images/articles/mask3.png" alt="news">
+                        </div>
+                        <div class="img-mobile">
+                            <img src="front/images/articles/mask_mobile.png" alt="news">
+                        </div>
+                    </div>
+                    <div class="latest-content">
+                        <div class="img-destop">
+                            <h4>Altigreen EV inaugurates its first dealership in Ahmedabad, Seventh outlet in India</h4>
+                        </div>
+                        <div class="img-mobile">
+                            <h4>Altigreen EV inaugurates its first dealership in Ahmedabad, Seventh outlet in India</h4>
+                        </div>
+                        <div class="date">
+                            <p>11-Nov-2022</p>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius Lorem ipsum dolor sit amet, consectetur.</p>
+                        <div class="lat-read-more-button">
+                            <a href="https://www.financialexpress.com/express-mobility/altigreen-ev-inaugurates-its-first-dealership-in-ahmedabad-seventh-outlet-in-india/2805344/" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                                                             alt="arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+                
                 <div class="latest-news-box">
                     <div class="images-box">
                         <div class="img-destop">
@@ -37,15 +63,40 @@
                             <h4>K S Cheema Group of Companies expands its wings in Gujarat</h4>
                         </div>
                         <div class="img-mobile">
-                            <h4>Autism care day</h4>
+                            <h4>K S Cheema Group of Companies expands its wings in Gujarat</h4>
                         </div>
                         <div class="date">
                             <p>26-Nov-2020</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Suspendisse varius Lorem ipsum dolor sit amet, consectetur.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius Lorem ipsum dolor sit amet, consectetur.</p>
                         <div class="lat-read-more-button">
-                            <a href="#">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                            <a href="https://www.thehitavada.com/Encyc/2020/11/26/K-S-Cheema-Group-of-Companies-expands-its-wings-in-Gujarat.html" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                                                             alt="arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="latest-news-box">
+                    <div class="images-box">
+                        <div class="img-destop">
+                            <img src="front/images/articles/mask3.png" alt="news">
+                        </div>
+                        <div class="img-mobile">
+                            <img src="front/images/articles/mask_mobile.png" alt="news">
+                        </div>
+                    </div>
+                    <div class="latest-content">
+                        <div class="img-destop">
+                            <h4>K S Metals & Mining Crushing Plant now operational</h4>
+                        </div>
+                        <div class="img-mobile">
+                            <h4>K S Metals & Mining Crushing Plant now operational</h4>
+                        </div>
+                        <div class="date">
+                            <p>27-Oct-2020</p>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius Lorem ipsum dolor sit amet, consectetur.</p>
+                        <div class="lat-read-more-button">
+                            <a href="https://www.thehitavada.com/Encyc/2020/10/27/K-S-Metals-Mining-Crushing-Plant-now-operational.html" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
                                                              alt="arrow"></span></a>
                         </div>
                     </div>
@@ -64,15 +115,14 @@
                             <h4>Nagpur offers huge biz opportunities: K S Cheema</h4>
                         </div>
                         <div class="img-mobile">
-                            <h4>Autism care day</h4>
+                            <h4>Nagpur offers huge biz opportunities: K S Cheema</h4>
                         </div>
                         <div class="date">
                             <p>28-Nov-2019</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Suspendisse varius Lorem ipsum dolor sit amet, consectetur.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius Lorem ipsum dolor sit amet, consectetur.</p>
                         <div class="lat-read-more-button">
-                            <a href="#">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                            <a href="https://www.thehitavada.com/Encyc/2019/11/28/Nagpur-offers-huge-biz-opportunities-K-S-Cheema.html" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
                                                              alt="arrow"></span></a>
                         </div>
                     </div>
@@ -88,17 +138,69 @@
                     </div>
                     <div class="latest-content">
                         <div class="img-destop">
-                            <h4>All New Mercedes GLC hits city roads</h4>
+                            <h4>K S Metals & Mining to take over UltraTech crushing plant</h4>
                         </div>
                         <div class="img-mobile">
-                            <h4>Autism care day</h4>
+                            <h4>K S Metals & Mining to take over UltraTech crushing plant</h4>
                         </div>
                         <div class="date">
-                            <p>13-Jan-2020</p>
+                            <p>28-Nov-2019</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius Lorem ipsum dolor.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius Lorem ipsum dolor sit amet, consectetur.</p>
                         <div class="lat-read-more-button">
-                            <a href="#">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                            <a href="https://www.thehitavada.com/Encyc/2019/11/28/K-S-Metals-Mining-to-take-over-UltraTech-crushing-plant.html" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                                                             alt="arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="latest-news-box">
+                    <div class="images-box">
+                        <div class="img-destop">
+                            <img src="front/images/articles/mask3.png" alt="news">
+                        </div>
+                        <div class="img-mobile">
+                            <img src="front/images/articles/mask_mobile.png" alt="news">
+                        </div>
+                    </div>
+                    <div class="latest-content">
+                        <div class="img-destop">
+                            <h4>Mercedes-Benz India opens two new showrooms in Nagpur</h4>
+                        </div>
+                        <div class="img-mobile">
+                            <h4>Mercedes-Benz India opens two new showrooms in Nagpur</h4>
+                        </div>
+                        <div class="date">
+                            <p>24-Apr-2019</p>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius Lorem ipsum dolor sit amet, consectetur.</p>
+                        <div class="lat-read-more-button">
+                            <a href="https://www.autocarpro.in/news-national/mercedesbenz-india-opens-two-new-showrooms-in-nagpur-42812" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                                                             alt="arrow"></span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="latest-news-box">
+                    <div class="images-box">
+                        <div class="img-destop">
+                            <img src="front/images/articles/mask3.png" alt="news">
+                        </div>
+                        <div class="img-mobile">
+                            <img src="front/images/articles/mask_mobile.png" alt="news">
+                        </div>
+                    </div>
+                    <div class="latest-content">
+                        <div class="img-destop">
+                            <h4>NRi of Powai Bags Hind Ratna Award</h4>
+                        </div>
+                        <div class="img-mobile">
+                            <h4>NRi of Powai Bags Hind Ratna Award</h4>
+                        </div>
+                        <div class="date">
+                            <p>24-Apr-2019</p>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius Lorem ipsum dolor sit amet, consectetur.</p>
+                        <div class="lat-read-more-button">
+                            <a href="https://www.autocarpro.in/news-national/mercedesbenz-india-opens-two-new-showrooms-in-nagpur-42812" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
                                                              alt="arrow"></span></a>
                         </div>
                     </div>
@@ -110,7 +212,7 @@
     <div class="articles-section">
         <div class="container">
             <div class="title">
-                <h3>ARTICLES</h3>
+                <h3>VIDEOS</h3>
             </div>
             <div class="articles-inner-section">
                 <div class="section-box-one">
@@ -125,17 +227,16 @@
                         </div>
                         <div class="latest-content">
                             <div class="img-destop">
-                                <h4>Altigreen EV inaugurates its first dealership in Ahmedabad, Seventh outlet in India</h4>
+                                <h4>Karnail Singh Cheema felicitated with Champions of Change Haryana Award</h4>
                             </div>
                             <div class="img-mobile">
-                                <h4>Autism care day</h4>
+                                <h4>Karnail Singh Cheema felicitated with Champions of Change Haryana Award</h4>
                             </div>
                             <div class="date">
                                 <p>November 11, 2022</p>
                             </div>
-                            <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet.</p>
                             <div class="lat-read-more-button">
-                                <a href="#">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                                <a href="https://youtu.be/n2jOXtXRJJk" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
                                                                  alt="arrow"></span></a>
                             </div>
                         </div>
@@ -151,17 +252,16 @@
                         </div>
                         <div class="latest-content">
                             <div class="img-destop">
-                                <h4>Autism care day</h4>
+                                <h4>K.S. Cheema speech at Hind Ratan Award</h4>
                             </div>
                             <div class="img-mobile">
-                                <h4>Autism care day</h4>
+                                <h4>K.S. Cheema speech at Hind Ratan Award</h4>
                             </div>
                             <div class="date">
                                 <p>15th Nov 2022</p>
                             </div>
-                            <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet.</p>
                             <div class="lat-read-more-button">
-                                <a href="#">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                                <a href="https://youtu.be/6Elwqmj971E" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
                                                                  alt="arrow"></span></a>
                             </div>
                         </div>
@@ -179,17 +279,16 @@
                         </div>
                         <div class="latest-content">
                             <div class="img-destop">
-                                <h4>This couple from Gujarat is the first owner of Mercedes-Benz EQS 580 4MATIC</h4>
+                                <h4>K.S. Cheema, Oman</h4>
                             </div>
                             <div class="img-mobile">
-                                <h4>Autism care day</h4>
+                                <h4>K.S. Cheema, Oman</h4>
                             </div>
                             <div class="date">
                                 <p>15th Nov 2022</p>
                             </div>
-                            <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet.</p>
                             <div class="lat-read-more-button">
-                                <a href="#">Read More <span><img src="front/images/benz-option/right-arrow.png"
+                                <a href="https://youtu.be/gZ4lI_ha-Us" target="_blank">Read More <span><img src="front/images/benz-option/right-arrow.png"
                                                                  alt="arrow"></span></a>
                             </div>
                         </div>
